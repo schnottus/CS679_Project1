045a75bc19be6dad9f5823ea0a897f914bd48ef1
unknown <slarson@cyclops-04.ad.cs.wisc.edu>
2012-09-16 16:22:57 -0500

    Now draws a circle and rectangle, the circle can be controlled by keyboard.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,60 @@
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+var WIDTH = 1200;
+var HEIGHT = 800;
+var canvas;
+var ctx;
+
+var x = 100;
+var y = 100;
+
+//circle function from http://html5.litten.com/moving-shapes-on-the-html5-canvas-with-the-keyboard/
+function circle(x,y,r) {
 ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
+ctx.arc(x, y, r, 0, Math.PI*2, true);
+ctx.fill();
+ctx.closePath();
+}
+
+//rectangle function from http://html5.litten.com/moving-shapes-on-the-html5-canvas-with-the-keyboard/
+function rect(x,y,w,h) {
+ctx.beginPath();
+ctx.rect(x,y,w,h);
+ctx.closePath();
+ctx.fill();
 ctx.stroke();
+}
+
+function init() {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  return setInterval(draw, 10);  //calls the draw function every 10 milliseconds
+}
+
+
+function doKeyDown(evt){
+	switch (evt.keyCode) {
+	case 38:  /* Up arrow was pressed */
+	y -= 10;
+	break;
+	case 40:  /* Down arrow was pressed */
+	y += 10;
+	break;
+	case 37:  /* Left arrow was pressed */
+	x -= 10;
+	break;
+	case 39:  /* Right arrow was pressed */
+	x += 10;
+	break;
+	}
+}
+
+function draw() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT); //erase everything on the canvas
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  rect(300,300,200,50);
+  ctx.fillStyle = "purple";
+  circle(x, y, 10);
+}
+
+init();
+window.addEventListener('keydown',doKeyDown,true);
