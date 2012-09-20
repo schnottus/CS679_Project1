adcc1e443c37d0ffa54a168dc0ec099a25166af2
Scott Larson <schnottus@gmail.com>
2012-09-20 05:48:28 -0500

    Added velocity properties to fish object and made functions to update and move flock, animated the flock by default

--- javascript.js
+++ javascript.js
@@ -6,10 +6,11 @@ var canvas;
 var ctx;
 
 var flock = [];
+var flockSpeed = 5;	//player fish speed
+var predSpeed = 4;  //predator speed
 
-
-var x = 100;
-var y = 100;
+//var x = 100;
+//var y = 100;
 
 //circle function from http://html5.litten.com/moving-shapes-on-the-html5-canvas-with-the-keyboard/
 function circle(x,y,r) {
@@ -28,6 +29,12 @@ ctx.fill();
 ctx.stroke();
 }
 
+//returns random number between 2 values (inclusive)
+function randFromTo(from,to)
+{
+    return Math.floor(Math.random()*(to-from +1) +from);
+}
+
 function printFlock(){ //flock troubleshooting
 	var str = "Flock array:";
 	
@@ -38,22 +45,53 @@ function printFlock(){ //flock troubleshooting
 	alert(str);
 }
 
-function Fish(x, y){
+function Fish(x, y, vX, vY, type){
+	this.type = type;   // 0 for player, 1 for regular fish, 2 for predator fish
 	this.x = x;
 	this.y = y;
+	this.vX = vX;
+	this.vY = vY;
 	this.draw = function(){
 		ctx.fillStyle = "purple";
         circle(this.x, this.y, 10);
 	}
+	this.move = function() {
+			this.x += this.vX;
+            this.y += this.vY;
+            if (this.x > canvas.width) {
+                if (this.vX > 0) {
+                    this.vX = -this.vX;
+                }
+            }
+            if (this.y > canvas.height) {
+                if (this.vY > 0) {
+                    this.vY = -this.vY;
+                }
+            }
+            if (this.x < 0) {
+                if (this.vX < 0) {
+                    this.vX = -this.vX;
+                }
+            }
+            if (this.y < 0) {
+                if (this.vY < 0) {
+                    this.vY = -this.vY;
+                }
+           }
+        }    
+	
 }
 //var myFish = new Fish(20, 20);
 //var myFish2 = new Fish(20, 40);
 
 function fillFlock(qty){
 	for (var i = 0; i < qty; i++){
-		var rx = Math.floor((Math.random()*WIDTH)+1);	//random x position between 1 and width
-		var ry = Math.floor((Math.random()*HEIGHT)+1);	//random y position between 1 and height
-		var tmp = new Fish(rx, ry);
+		var type = 1;
+		var rx = randFromTo(1, WIDTH);	//random x position between 1 and width
+		var ry = randFromTo(1, HEIGHT);	//random y position between 1 and height
+		var rvX = randFromTo(1, flockSpeed);  //random x velocity between 1 and speed
+		var rvY = randFromTo(1, flockSpeed);  //random y velocity between 1 and speed
+		var tmp = new Fish(rx, ry, rvX, rvY, type);
 		flock.push(tmp);  //add new fish to end of flock array
 	}
 
@@ -65,18 +103,30 @@ function renderFlock(){
 		flock[i].draw();
 	}
 }
-	
 
+
+function updateFlock(){
+	var size = flock.length;
+	for(var i = 0; i < size; i++){
+		flock[i].move();
+	}
+}
+	
+function updateGame(){
+	updateFlock();
+	//updatePredators
+	//update user interface (health, score, etc)
+}
 
 function init() {
-  canvas = document.getElementById("myCanvas");
-  ctx = canvas.getContext("2d");
+	canvas = document.getElementById("myCanvas");
+	ctx = canvas.getContext("2d");
+	ctx.fillStyle = "white";
+	ctx.strokeStyle = "black";
+	fillFlock(10);
   
-  fillFlock(10);
-  
-  return setInterval(draw, 10);  //calls the draw function every 10 milliseconds
-  
-  
+	//change to request animation frame
+	return setInterval(gameLoop, 10);  //calls the gameLoop function every 10 milliseconds
 }
 
 
@@ -98,19 +148,18 @@ function doKeyDown(evt){
 	}
 }
 
-function draw() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT); //erase everything on the canvas
-  ctx.fillStyle = "white";
-  ctx.strokeStyle = "black";
-  //rect(300,300,200,50);
-
-  //myFish.draw();
-  //myFish2.draw();
-  
-  renderFlock();
+function gameLoop() {
+	updateGame();  //update game pieces, score, etc
+	
+	ctx.clearRect(0, 0, WIDTH, HEIGHT); //erase everything on the canvas
+	
+	//draw background image
+	renderFlock();  //draw regular fish (flock)
+	//draw predators
 }
 
 window.onload = function(){
 init();
 }
+
 window.addEventListener('keydown',doKeyDown,true);
